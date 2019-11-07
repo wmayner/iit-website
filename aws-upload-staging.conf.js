ce430a8dcae0268ef096989bb9e687118e574a19
Will Mayner <wmayner@gmail.com>
2019-11-06 21:04:14 -0600

    Code formatting

--- aws-upload-staging.conf.js
+++ aws-upload-staging.conf.js
@@ -1,12 +1,13 @@
 module.exports = {
-    credentials: "../aws-credentials.json",
-    bucketName: "staging.integratedinformationtheory.org",
-    patterns: [
-        "*.html"
-      , "img/**/*.png"
-      , "css/**/*.css"
-      , "js/**/*.js"
-      , "download/*"
-      , "publications/*"
-    ]
+  credentials: "../aws-credentials.json",
+  bucketName: "staging.integratedinformationtheory.org",
+  patterns: [
+      "*.html"
+    , "img/**/*.png"
+    , "css/**/*.css"
+    , "js/**/*.js"
+    , "download/*"
+    , "publications/*"
+    , "actual-causation/*"
+  ]
 }
